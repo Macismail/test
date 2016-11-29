e2be5bdd892afc9d89da84b3d05894ad02e01c8d
macismail7 <macismail7@gmail.com>
2016-11-29 11:43:28 -0500

    smail1

--- page1.php
+++ page1.php
@@ -2,10 +2,20 @@
 <html>
 	<head>
 		<title> PHP </title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+					integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
+					integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 	</head>
 	<body>
 		<div>
@@ -14,28 +24,42 @@
 		<h1> My first PHP!</h1>
 		
 		<?php
-		
-			echo $_POST ['fName'];
-			echo ' </br>';
-			echo $_POST ['lName'];
-			echo ' </br>';
-			echo $_POST ['bTitle'];
-			echo ' </br>';
+			$xmlDOM = new DOMDocument();
+			$xmlDOM -> load('book.xml');
+			$x = $xmlDOM -> documemntelement();
+			
+			foreach ($x ->childNodes as $item){
+				
+				print $item -> nodeName. " = ".$item ->nodeValue . "</br> ";
+			}
+				
+			$xslDOM = new DOMDocument();
+			$xslDOM -> substitutesEntities=true;
+			$xslDOM -> load('book.xsl');
+			
+			// Configure the transformer
+			$proc = new XSLTProcessor;
+			$proc->importStyleSheet($xsl);
+			
+			echo $proc->transformToXML($xml);
+			
+			if(isset($_SERVER['HTTP_REFERER'])){
+   		header("Location: " . $_SERVER['HTTP_REFERER']);
+			} else {
+   		echo "An Error";
+			}
+			
+			//get details from form
+			$author = $_Post['author'];
+			$title = $_Post['title'];
+			$genre = $_Post['genre'];
+			$price = $_Post['price'];
+			
+			echo "book.xsl";
 	
 		?> 
 		
-		<form method="post" action="page1.php">
-			
-			First Name:	<input type="text" name="fName" ></br></br>
-			Last Name: <input type="text" name="lName" ></br></br>
-			Favorite Book Title:
-			<input name="bTitle" type="text"></br></br><br>
-				
-			<input type="submit" name="submit" value="Submit">
-			<input type="reset" name="reset">
-			
-		</form>
-		</div>
+		
 			
 	</body>
 </html>
