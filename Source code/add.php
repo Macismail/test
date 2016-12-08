6d58cd4e831ade579a7f252eda20b324364f30cc
darhq <dariuszhanuszewicz001@gmail.com>
2016-12-08 12:27:36 +0000

    Add files via upload
    This is source code which I used as a base to create our CRUD. It works in local host, it does not want to work in codanywhere.

---
+++ Source code/add.php
@@ -0,0 +1,30 @@
+<?php
+if(isset($_POST['submitSave'])){
+	$products=simplexml_load_file('products.xml');
+	$product=$products->addChild('product');
+	$product->addAttribute('id', $_POST['id']);
+	$product->addChild('name', $_POST['name']);
+	$product->addChild('price', $_POST['price']);
+	file_put_contents('products.xml',$products->asXML());
+	header('location:index.php');
+}
+?>	
+<form method="post">
+	<table cellpading="2" cellspacing="2">
+		<tr>
+			<td>ID</td>
+			<td><input type="text" name="id"></td>
+		</tr>
+		<tr>
+			<td>Name</td>
+			<td><input type="text" name="name"></td>
+		</tr>
+		<tr>
+			<td>Price</td>
+			<td><input type="text" name="price"></td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" name="submitSave" value="Save"></td>
+		</tr>
+</form>
